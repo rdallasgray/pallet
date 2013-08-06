60a4b3ff5a05265f0d8cfe012b12a2c60b4bd8ed
Robert Dallas Gray <mail@robertdallasgray.com>
2013-08-06 20:32:35 +0100

    0.1.20

--- lib/pallet-pkg.el
+++ lib/pallet-pkg.el
@@ -1,3 +1,3 @@
-(define-package "pallet" "0.1.16"
+(define-package "pallet" "0.1.20"
   "A package manager for Emacs, using Cask."
   '((cask "0.4.1")))
--- lib/pallet.el
+++ lib/pallet.el
@@ -4,7 +4,7 @@
 
 ;; Author: Robert Dallas Gray
 ;; URL: https://github.com/rdallasgray/pallet
-;; Version: 0.1.16
+;; Version: 0.1.20
 ;; Created: 2013-02-24
 ;; Keywords: elpa, package
 

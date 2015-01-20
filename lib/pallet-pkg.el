a33186da0d803bee85830f2899c81b974ae04f44
Robert Dallas Gray <mail@robertdallasgray.com>
2015-01-20 10:12:05 +0000

    Make v0.9.0

--- lib/pallet-pkg.el
+++ lib/pallet-pkg.el
@@ -1,4 +1,4 @@
-(define-package "pallet" "0.8.0" "A package management tool for Emacs, using Cask."
+(define-package "pallet" "0.9.0" "A package management tool for Emacs, using Cask."
   '((s "1.9.0")
     (f "0.17.1")
     (cask "0.7")))
